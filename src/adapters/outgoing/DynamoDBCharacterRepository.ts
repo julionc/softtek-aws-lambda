274ec91bb916b70c458f83818c5827ceb99c7f81
Julio Napurí <julionc@gmail.com>
2024-11-29 10:59:28 -0500

    add ttl to character table

--- src/adapters/outgoing/DynamoDBCharacterRepository.ts
+++ src/adapters/outgoing/DynamoDBCharacterRepository.ts
@@ -6,8 +6,13 @@ import { marshall, unmarshall } from "@aws-sdk/util-dynamodb";
 export class DynamoDBCharacterRepository implements CharacterRepository {
   private tableName = process.env.CHARACTER_TABLE!;
   private client = new DynamoDBClient({ region: process.env.AWS_DEFAULT_REGION });
+  private cacheDurationInSeconds = parseInt(process.env.TABLE_TTL || "30") * 60;
 
   async save(character: Character): Promise<void> {
+    const ttl = this.cacheDurationInSeconds
+      ? Math.floor(Date.now() / 1000) + this.cacheDurationInSeconds
+      : undefined;
+
     const params = {
       TableName: this.tableName,
       Item: {
@@ -21,6 +26,7 @@ export class DynamoDBCharacterRepository implements CharacterRepository {
         birthYear: { S: character.birthYear },
         gender: { S: character.gender },
         homeworld: { S: character.homeworld },
+        ...(ttl && { ttl: { N: ttl.toString() } }),
       },
       // Item: marshall({ character })
     };
